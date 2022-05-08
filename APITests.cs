2bdd1e205c3919a8827b61616c7aa46d27569da3
Bert Ulrich <bert.ulrich@gmail.com>
2022-05-08 07:25:04 -0400

    JSON deserializer

--- APITests.cs
+++ APITests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
-
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace SeleniumApp1
 {
@@ -11,6 +12,15 @@ namespace SeleniumApp1
             TestAPI api = new TestAPI();
 
             var response = await api.testRequest();
+            //var jsonString = JsonSerializer.Serialize(response);
+
+            //Assert.That(jsonString, Is.EqualTo("animals"));
+            Assert.IsNotNull(response);
+
+            Animals animals = JsonSerializer.Deserialize<Animals>(response);
+
+            Assert.AreEqual(26, animals.entries.Length);
+            Assert.AreEqual(26, animals.count);
 
 
         }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SeleniumApp1
+
+Added deserialize JSON response and assertions.
+
+Added REST API request and test.  Confirmed JSON request and response are correct.
+
 Selenium framework using C#, Page Object Model, NUnit.
